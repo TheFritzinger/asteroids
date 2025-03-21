c57919204a9c558d291f1234b3bdfc656a01d8c4
TheFritzinger <Fritzbay@proton.me>
2025-03-21 16:35:05 +0100

    fixed player shooting speed

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     
     def triangle(self):
@@ -24,19 +25,23 @@ class Player(CircleShape):
     
     
     def rotate(self, dt):
-        self.rotation += PLAYER_TURNSPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt   
     
     def shoot(self):
+        if self.shoot_timer > 0:
+            return 
         shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOTSPEED
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
 
         
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
